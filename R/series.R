badb4edbe53466e103d2818c2b69f7548ca95b83
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-11-29 11:49:45 +0000

    Serie

--- R/series.R
+++ R/series.R
@@ -4,12 +4,6 @@
 # Project Director: Carlos J. Perez Gonzalez <cpgonzal@ull.es>
 #
 
-# listar_operaciones_series
-# Return all operations available
-listar_operaciones_series <- function() {
-  return(fromJSON("http://servicios.ine.es/wstempus/js/ES/OPERACIONES_DISPONIBLES"))
-}
-
 # obtener_serie
 # Obtiene una serie a partir del código identificativo de la serie
 obtener_serie <- function(codigo) {

89b38a92f6becc8718611054016eb1163e557c1e
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-11-29 14:37:49 +0000

    Series

--- R/series.R
+++ R/series.R
@@ -6,13 +6,20 @@
 
 # obtener_serie
 # Obtiene una serie a partir del código identificativo de la serie
-obtener_serie <- function(codigo) {
-  url <- paste0("http://servicios.ine.es/wstempus/js/ES/SERIE/", codigo)
+obtener_serie <- function(cod) {
+  url <- paste0("http://servicios.ine.es/wstempus/js/ES/SERIE/", cod)
   return(fromJSON(url))
 }
 
+# listar_series_operacion
+# Obtiene series de una operación
+listar_series_operacion <- function(cod) {
+  url <- paste0("http://servicios.ine.es/wstempus/js/ES/SERIES_OPERACION/", cod)
+  return(fromJSON(url))
+}
 
 # Example of usage
+# list_serie_op <- listar_series_operacion("IPC")
 # ob_serie <- obtener_serie("IPC206449")
 
 

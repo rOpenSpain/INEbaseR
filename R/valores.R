980b2f354223e9b2922985dcd979f6e6edb4c38a
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-11-30 11:16:28 +0000

    Valores det

--- R/valores.R
+++ R/valores.R
@@ -8,7 +8,7 @@
 # Información de los valores que puede tomar la variable: identificador Tempus3 del valor, identificador Tempus 3 de la variable a la que pertenece, nombre del valor y código oficial.
 # [?parámetros] = posibilidad de usar det=1 para ver el detalle de la variable a la que pertenece.
 listar_valores_all <- function(id, det = 0){
-  if ((det > 1) || (det < 0))
+  if ((det < 0) || (det > 1))
     stop("Ha definido el parámetro 'det' con un valor incorrecto.")
   url <- paste0("http://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLE/", id, "?det=", det)
   return(fromJSON(url))
@@ -18,7 +18,7 @@ listar_valores_all <- function(id, det = 0){
 # Información de los valores que puede tomar la variable para describir la operación: identificador Tempus3 del valor, objeto variable Tempus3 a la que pertenece, nombre del valor y código oficial.
 # [?parámetros] = posibilidad de usar det=1 para ver el detalle de la variable a la que pertenece.
 listar_valores_variableoperacion <- function(id, op, det = 0) {
-  if ((det > 1) || (det < 0))
+  if ((det < 0) || (det > 1))
     stop("Ha definido el parámetro 'det' con un valor incorrecto.")
   url <- paste0("http://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLEOPERACION/", id, "/", op, "?det=", det)
   return(fromJSON(url))

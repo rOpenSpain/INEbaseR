b9a3e0ef8bdefc30a6d02c6995a6096967a7bb60
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-12-10 11:57:03 +0000

    Valores

--- R/valores.R
+++ R/valores.R
@@ -4,28 +4,37 @@
 # Project Director: Carlos J. Perez Gonzalez <cpgonzal@ull.es>
 #
 
-# listar_valores_all
-# Información de los valores que puede tomar la variable: identificador Tempus3 del valor, identificador Tempus 3 de la variable a la que pertenece, nombre del valor y código oficial.
+# get_values_all
+# Obtiene todos los valores de una variable.
+# Consulta: Valores de la variable “Provincias”.
 # [?parámetros] = posibilidad de usar det=1 para ver el detalle de la variable a la que pertenece.
-listar_valores_all <- function(id, det = 0) {
+get_values_all <- function(id, det = 0, lang = "ES") {
   if ((det < 0) || (det > 1))
-    stop("Ha definido el parámetro 'det' con un valor incorrecto.")
-  url <- paste0("http://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLE/", id, "?det=", det)
+    stop("You have defined 'det' parameter with an incorrect value.")
+  url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/VALORES_VARIABLE/", id, "?det=", det)
   return(fromJSON(url))
 }
 
-# listar_valores_variableoperacion
-# Información de los valores que puede tomar la variable para describir la operación: identificador Tempus3 del valor, objeto variable Tempus3 a la que pertenece, nombre del valor y código oficial.
+# get_values_variableoperation
+# Obtiene todos los valores de una variable para una operación.
 # [?parámetros] = posibilidad de usar det=1 para ver el detalle de la variable a la que pertenece.
-listar_valores_variableoperacion <- function(id, op, det = 0) {
+get_values_variableoperation <- function(id, op, det = 0, ioe = FALSE, lang = "ES") {
   if ((det < 0) || (det > 1))
-    stop("Ha definido el parámetro 'det' con un valor incorrecto.")
-  url <- paste0("http://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLEOPERACION/", id, "/", op, "?det=", det)
+    stop("You have defined 'det' parameter with an incorrect value.")
+  if (ioe)
+    url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/VALORES_VARIABLEOPERACION/", id, "/IOE", op, "?det=", det)
+  else
+    url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/VALORES_VARIABLEOPERACION/", id, "/", op, "?det=", det)
   return(fromJSON(url))
 }
 
 # Example of usage
-# list_val <- listar_valores_all(115)
-# list_val <- listar_valores_all(115, 1)
-# list_val_var_op <- listar_valores_variableoperacion(762, 25)
-# list_val_var_op <- listar_valores_variableoperacion(762, 25, 1)
+# library(jsonlite)
+# Nota: estos id (115) se obtienen de las variables ("Provincias")
+# list_val <- get_values_all(115) # "det" no definido.
+# list_val <- get_values_all(115, 1) # det = 1
+# Nota: el "id" (762) se obtiene del código id de variables, y "op" (25) es el código de las operaciones
+# list_val_var_op <- get_values_variableoperation(762, 25)
+# list_val_var_op <- get_values_variableoperation(762, 25, 1)
+# list_val_var_op <- get_values_variableoperation(762, 30138, ioe = TRUE)
+# list_val_var_op <- get_values_variableoperation(762, 30138, ioe = TRUE, 1)

4c4c182c5a6da78900b25c8fc3df6fa201968ffc
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-12-10 11:33:33 +0000

    Variables

--- R/variables.R
+++ R/variables.R
@@ -4,28 +4,29 @@
 # Project Director: Carlos J. Perez Gonzalez <cpgonzal@ull.es>
 #
 
-# listar_variables_all
-# Información de todas las variables del Sistema: identificador Tempus3, nombre de la variable y código oficial.
-listar_variables_all <- function() {
-  return(fromJSON("http://servicios.ine.es/wstempus/js/ES/VARIABLES"))
+# get_variables_all
+# Obtiene todas las variables del Sistema.
+get_variables_all <- function(lang = "ES") {
+  return(fromJSON(paste0("http://servicios.ine.es/wstempus/js/", lang, "/VARIABLES")))
 }
 
-# listar_variables_operacion
-# Información de las variables que describen la operación: identificador Tempus3, nombre de la variable y código oficial.
-listar_variables_operacion <- function(cod) {
-  url <- paste0("http://servicios.ine.es/wstempus/js/ES/VARIABLES_OPERACION/", cod)
-  return(fromJSON(url))
-}
-
-# listar_variables_operacion_ioe
-# Obtiene un data frame con las variables operación a partir del IOE (Inventario de Operaciones Estadísticas)
-listar_variables_operacion_ioe <- function(cod_ioe) {
-  url <- paste0("http://servicios.ine.es/wstempus/js/ES/VARIABLES_OPERACION/IOE", cod_ioe)
+# get_variable
+# Obtiene un data frame con las variables para una operación a partir de:
+#   Código numérico (id)
+#   código alfabético (IPC)
+#   código IOE (Inventario de Operaciones Estadísticas)
+get_variable <- function(code, ioe = FALSE, lang = "ES") {
+  if (ioe)
+    url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/VARIABLES_OPERACION/IOE", code)
+  else
+    url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/VARIABLES_OPERACION/", code)
   return(fromJSON(url))
 }
 
 # Example of usage
 # library(jsonlite)
-# list_vars <- listar_variables_all()
-# list_vars_op <- listar_variables_operacion("IPC")
-# list_vars_op_ioe <- listar_variables_operacion_ioe("30138")
+# list_var_all <- get_variables_all()
+# Nota: estos códigos se obtienen de las operaciones
+# list_var_id <- get_variable(25)
+# list_var_cod <- get_variable("IPC")
+# list_var_ioe <- get_variable(30138, ioe = TRUE)

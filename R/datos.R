aa4370c37cd09a5e253b4ab2466ca0645fec74da
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-12-12 18:42:45 +0000

    Datos

--- R/datos.R
+++ R/datos.R
@@ -42,6 +42,37 @@ get_data_table <- function(id, nult = 0, det = 0, tip = NA, lang = "ES") {
   return(fromJSON(url))
 }
 
+# DATOS_METADATAOPERACION
+# Obtiene los "n" últimos datos de series mediante el cruce de metadatos
+# {?parámetros}= posibilidad de usar:
+#   det=2 para ver dos niveles de detalle, en concreto para poder acceder a los atributos del objeto dato.
+#   tip=AM para obtener los metadatos (cruce variables-valores) de la serie y una salida de tipo amigable.
+get_data_metadataoperation <- function(id, g1var = 0, g1val = 0, g2var = 0, g2val = 0, g3var = 0, p = 0, nult = 0, det = 0, tip = NA, ioe = FALSE, lang = "ES") {
+  if ((det < 0) || (det > 2))
+    stop("You have defined 'det' parameter with an incorrect value.")
+  if (p < 0)
+    stop("You have defined 'p' (periodicity) parameter with an incorrect value.")
+  if ((tip != "AM") || (is.na(tip)))
+    stop("You have defined 'tip' parameter with an incorrect value.")
+  if ((g1var <= 0) || (g1val <= 0))
+    stop("You have to insert a 'g1' parameter with a correct value.")
+  if ((g2var <= 0) || (g2val <= 0))
+    stop("You have to insert a 'g2' parameter with a correct value.")
+  if (g3var <= 0)
+    stop("You have to insert a 'g3' parameter with a correct value.")
+  if (ioe)
+    if (nult == 0)
+      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/IOE", id, "?g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
+    else
+      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/IOE", id, "?nult=", nult, "&g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
+  else
+    if (nult == 0)
+      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/", id, "?g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
+    else
+      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/", id, "?nult=", nult, "&g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
+  return(fromJSON(url))
+}
+
 # Example of usage
 # library(jsonlite)
 # DATOS_SERIE:
@@ -53,5 +84,9 @@ get_data_table <- function(id, nult = 0, det = 0, tip = NA, lang = "ES") {
 # list_datos_serie <- get_data_serie("IPC206449", "2013-01-01", "2016-01-01")
 # Obtener datos de una serie a partir de una fecha
 # list_datos_serie <- get_data_serie("IPC206449", "2010-01-01")
+# DATOS_TABLA:
 # Obtener los n últimos datos de una tabla
 # list_datos_tabla <- get_data_table(22350, 4)
+# DATOS_METADATAOPERACION:
+# Obtener n los últimos datos de series mediante el cruce de metadatos
+# list_datos_metaop <- get_data_metadataoperation("IPC", 115, 29, 3, 84, 762, p = 1, nult = 1)

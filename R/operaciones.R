bcfb730ce383eec92f915864734a971f1e997c2a
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2017-12-15 18:47:58 +0000

    operations roxygen2 documentation

--- R/operaciones.R
+++ R/operaciones.R
@@ -2,19 +2,31 @@
 #
 # Author: Andres Nacimiento Garcia <andresnacimiento@gmail.com>
 # Project Director: Carlos J. Perez Gonzalez <cpgonzal@ull.es>
-#
 
-# get_operations_all
-# Obtiene un data frame con todas las operaciones estadísticas disponibles
+#' @title Get operations (all)
+#' @description This function returns a data frame with all available statistical operations
+#' @param lang language used to obtain information
+#' @examples
+#' get_operations_all()
+#' @export
 get_operations_all <- function(lang = "ES") {
   return(fromJSON(paste0("http://servicios.ine.es/wstempus/js/", lang, "/OPERACIONES_DISPONIBLES")))
 }
 
-# get_operation
-# Obtiene un data frame a partir de:
-#   Código numérico (id)
-#   código alfabético (IPC)
-#   código IOE (Inventario de Operaciones Estadísticas)
+#' @title Get operation
+#' @description This function returns a data frame with all available statistical operations of an id or code
+#' @param code to find an operation
+#' @param ioe \code{TRUE} if code is in format \code{IO30138}, and \code{FALSE} by default
+#' @param lang language used to obtain information
+#' @details
+#' Numeric code (id)
+#' Alphabetic code (IPC)
+#' IOE code (Inventario de Operaciones Estadísticas)
+#' @examples
+#' get_operation(25)
+#' get_operation("IPC")
+#' get_operation(30138, ioe = TRUE)
+#' @export
 get_operation <- function(code, ioe = FALSE, lang = "ES") {
   if (ioe)
     url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/OPERACION/IOE", code)
@@ -22,10 +34,3 @@ get_operation <- function(code, ioe = FALSE, lang = "ES") {
     url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/OPERACION/", code)
   return(fromJSON(url))
 }
-
-# Example of usage
-# library(jsonlite)
-# list_op_all <- get_operations_all()
-# list_op_id <- get_operation(25)
-# list_op_cod <- get_operation("IPC")
-# list_op_ioe <- get_operation(30138, ioe = TRUE)

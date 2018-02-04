b9ebdebafdabad2dba7a79c3b009c7913eafea84
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2018-02-04 19:10:23 +0000

    Get series metadataoperation

--- R/series.R
+++ R/series.R
@@ -20,6 +20,7 @@ get_serie <- function(code, det = 0, tip = NA, lang = "ES") {
     stop("You have defined 'tip' parameter with an incorrect value.")
   url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/SERIE/", code, "?det=", det, "&tip=", tip)
   return(fromJSON(url))
+  fromJSON(paste0("http://servicios.ine.es/wstempus/js/", lang, "/SERIE/", code, "?det=", det, "&tip=", tip))
 }
 
 #' @title Get series operation
@@ -88,12 +89,8 @@ get_series_table <- function(id, det = 0, tip = NA, lang = "ES") {
 #' @title Get series metadata operation
 #' @description This function returns a data frame with all series by metadata crossing from an id or code
 #' @param code operation identification
-#' @param g1var variable (g1)
-#' @param g1val valor (g1)
-#' @param g2var variable (g2)
-#' @param g2val valor (g2)
-#' @param g3var variable (g3)
-#' @param p periodicidad
+#' @param query string separated by \code{AND} with syntax \code{variable = value} using natural language
+#' @param p periodicity, \code{p = 1} by default
 #' @param det \code{det = 2} to see two levels of depth, specifically to access the \code{PubFechaAct} object, \code{det = 0} by default
 #' @param tip \code{tip = M} to obtain the metadata (crossing variables-values) of the series.
 #' @param ioe \code{TRUE} if code is in format \code{IO30138}, and \code{FALSE} by default
@@ -108,24 +105,62 @@ get_series_table <- function(id, det = 0, tip = NA, lang = "ES") {
 #' Grupos ECOICOP (FK_VARIABLE=762) = "Todos los grupos ECOICOP” (FK_VALOR=null) >  g3=762:
 #' Serie mensual (FK_PERIODICIDAD=1) >  p=1  (Ver PUBLICACIONES_OPERACION)
 #' @examples
-#' get_series_metadataoperation("IPC", 115, 29, 3, 84, 762, 1)
+#' get_series_metadataoperation("IPC", query = "Provincias = Madrid AND Tipo de dato = Variación mensual AND Grupos ECOICOP = NULL")
 #' @export
-get_series_metadataoperation <- function(id, g1var = 0, g1val = 0, g2var = 0, g2val = 0, g3var = 0, p = 0, det = 0, tip = NA, ioe = FALSE, lang = "ES"){
+get_series_metadataoperation <- function(code, query = NA, p = 1, det = 0, tip = NA, ioe = FALSE, lang = "ES"){
+
   if ((det < 0) || (det > 2))
     stop("You have defined 'det' parameter with an incorrect value.")
-  if (p < 0)
+  if (p <= 0)
     stop("You have defined 'p' (periodicity) parameter with an incorrect value.")
   if ((tip != "M") && (!is.na(tip)))
     stop("You have defined 'tip' parameter with an incorrect value.")
-  if ((g1var <= 0) || (g1val <= 0))
-    stop("You have to insert a 'g1' parameter with a correct value.")
-  if ((g2var <= 0) || (g2val <= 0))
-    stop("You have to insert a 'g2' parameter with a correct value.")
-  if (g3var <= 0)
-    stop("You have to insert a 'g3' parameter with a correct value.")
+  if (!is.na(query))
+    stop("You have defined 'query' parameter with an incorrect value.")
+
+  # Split query
+  df_queries <- NULL
+  queries <- strsplit(query, split = "AND")
+  for (queries_splited in queries){
+    var_val <- strsplit(queries_splited, split = "=")
+    for (string in var_val){
+      df_queries <- rbind(df_queries, data.frame(string))
+    }
+  }
+
+  # Get variables and values id
+  variables <- get_variables_operation(code)
+  result <- NULL
+  count <- 0
+  g <- 1
+  for (qvalue in df_queries$string){
+    qvalue <- trimws(as.character(qvalue))
+    if (count %% 2 == 0) { # variables
+      variableId <- variables[match(qvalue, variables[["Nombre"]]),][["Id"]]
+      variable <- paste0("g", g, "=", variableId, ":")
+      result <- rbind(result, variable)
+      g <- g + 1
+    } else { # values
+      value <- get_values_variableoperation(variableId, code)
+      value <- value[match(qvalue, value[["Nombre"]]),][["Id"]]
+      if (is.na(value)){
+        value <- ""
+      }
+      value <- paste0(value, "&")
+      result <- rbind(result, value)
+    }
+    count <- count + 1
+  }
+
+  # Join query
+  urlStr <- paste0(result, collapse = "")
+
   if (ioe)
-    url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/SERIE_METADATAOPERACION/IOE", id, "?g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
+    url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/SERIE_METADATAOPERACION/IOE", code, "?", urlStr, "p=", p, "&tip=", tip, "&det=", det)
   else
-    url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/SERIE_METADATAOPERACION/", id, "?g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
+    url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/SERIE_METADATAOPERACION/", code, "?", urlStr, "p=", p, "&tip=", tip, "&det=", det)
   return(fromJSON(url))
 }
+
+tmp <- get_series_metadataoperation("IPC", query = "Provincias = Madrid AND Tipo de dato = Variación mensual AND Grupos ECOICOP = NULL")
+

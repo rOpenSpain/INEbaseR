244aeb6573074cb025ad055c23cd0948a671c937
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2018-03-14 12:37:46 +0000

    Improved get data metadataoperation

--- R/datos.R
+++ R/datos.R
@@ -61,13 +61,9 @@ get_data_table <- function(id, nult = 0, det = 0, tip = NA, lang = "ES") {
 
 #' @title Get data metadata-operation
 #' @description This function returns a data frame with latest \code{n} series data by crossing metadata from an id or code
-#' @param id table identification
-#' @param g1var variable (g1)
-#' @param g1val valor (g1)
-#' @param g2var variable (g2)
-#' @param g2val valor (g2)
-#' @param g3var variable (g3)
-#' @param p periodicidad
+#' @param code table identification
+#' @param query string separated by \code{AND} with syntax \code{variable = value} using natural language
+#' @param p periodicity
 #' @param nult last \code{n} values
 #' @param det \code{det = 2} to see two levels of depth, specifically to access the \code{PubFechaAct} object, \code{det = 0} by default
 #' @param tip \code{tip = AM} to obtain the metadata (crossing variables-values) of the series and a friendly output.
@@ -75,29 +71,62 @@ get_data_table <- function(id, nult = 0, det = 0, tip = NA, lang = "ES") {
 #' @param lang language used to obtain information
 #' @examples
 #' get_data_metadataoperation("IPC", 115, 29, 3, 84, 762, p = 1, nult = 1)
+#' get_data_metadataoperation("IPC", query = "Provincias = Madrid AND Tipo de dato = Variación mensual AND Grupos ECOICOP = NULL", nult = 1)
 #' @export
-get_data_metadataoperation <- function(id, g1var = 0, g1val = 0, g2var = 0, g2val = 0, g3var = 0, p = 0, nult = 0, det = 0, tip = NA, ioe = FALSE, lang = "ES") {
+get_data_metadataoperation <- function(code, query = NULL, p = 1, nult = 1, det = 0, tip = NA, ioe = FALSE, lang = "ES") {
+
   if ((det < 0) || (det > 2))
     stop("You have defined 'det' parameter with an incorrect value.")
   if (p < 0)
     stop("You have defined 'p' (periodicity) parameter with an incorrect value.")
-  if ((tip != "AM") || (is.na(tip)))
+  if ((tip != "AM") && (!is.na(tip)))
     stop("You have defined 'tip' parameter with an incorrect value.")
-  if ((g1var <= 0) || (g1val <= 0))
-    stop("You have to insert a 'g1' parameter with a correct value.")
-  if ((g2var <= 0) || (g2val <= 0))
-    stop("You have to insert a 'g2' parameter with a correct value.")
-  if (g3var <= 0)
-    stop("You have to insert a 'g3' parameter with a correct value.")
+  if (nult < 1)
+    stop("You have defined 'nult' parameter with an incorrect value.")
+
+  # Split query
+  df_queries <- NULL
+  queries <- strsplit(query, split = "AND")
+  for (queries_splited in queries){
+    var_val <- strsplit(queries_splited, split = "=")
+    for (string in var_val){
+      df_queries <- rbind(df_queries, data.frame(string))
+    }
+  }
+
+  # Get variables and values id
+  variables <- get_variables_operation(code)
+  result <- NULL
+  count <- 0
+  g <- 1
+  for (qvalue in df_queries$string){
+    qvalue <- trimws(as.character(qvalue))
+    if (count %% 2 == 0) { # variables
+      variableId <- variables[match(qvalue, variables[["Nombre"]]),][["Id"]]
+      variable <- paste0("g", g, "=", variableId, ":")
+      result <- rbind(result, variable)
+      g <- g + 1
+    } else { # values
+      value <- get_values_variableoperation(variableId, code)
+      value <- value[match(qvalue, value[["Nombre"]]),][["Id"]]
+      if (is.na(value)){
+        value <- ""
+      }
+      value <- paste0(value, "&")
+      result <- rbind(result, value)
+    }
+    count <- count + 1
+  }
+
+  # Join query
+  urlStr <- paste0(result, collapse = "")
+
+  # URL definition
   if (ioe)
-    if (nult == 0)
-      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/IOE", id, "?g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
-    else
-      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/IOE", id, "?nult=", nult, "&g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
+      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/IOE", code, "?", urlStr, "p=", p, "&tip=", tip, "&det=", det, "&nult=", nult)
   else
-    if (nult == 0)
-      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/", id, "?g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
-    else
-      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/", id, "?nult=", nult, "&g1=", g1var, ":", g1val, "&g2=", g2var, ":", g2val, "&g3=", g3var, ":&p=" , p, "&tip=", tip, "&det=", det)
+      url <- paste0("http://servicios.ine.es/wstempus/js/", lang, "/DATOS_METADATAOPERACION/", code, "?", urlStr, "p=", p, "&tip=", tip, "&det=", det, "&nult=", nult)
+
+  print(url)
   return(fromJSON(url))
 }

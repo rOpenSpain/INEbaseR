b9ad77c26f0d443fde3bbaf1edca6623cac7418d
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2018-04-13 18:47:09 +0100

    FIXED: cache directory

--- R/cache.R
+++ R/cache.R
@@ -3,12 +3,21 @@
 # Author: Andres Nacimiento Garcia <andresnacimiento@gmail.com>
 # Project Director: Carlos J. Perez Gonzalez <cpgonzal@ull.es>
 
+build_cache_directory <- function(path = "cache") {
+  if (!dir.exists(path)) {
+    dir.create(path, showWarnings = TRUE, recursive = FALSE, mode = "0755")
+  }
+}
+
 check_cache <- function(data_type, code){
   file_name <- paste0("cache/", data_type, "_", code, ".RData")
   file_test("-f", file_name)
 }
 
 build_cache <- function(data, data_type, code){
+
+  build_cache_directory()
+
   if (check_cache(data_type, code)){
     clean_cache(data_type, code)
   }

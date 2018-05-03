088165ebcb4537ba1360a9fe14a6cdcb9f440d56
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2018-05-03 11:54:38 +0100

    ADDED: date in cache filename

--- R/cache.R
+++ R/cache.R
@@ -3,6 +3,11 @@
 # Author: Andres Nacimiento Garcia <andresnacimiento@gmail.com>
 # Project Director: Carlos J. Perez Gonzalez <cpgonzal@ull.es>
 
+get_cache_file_name <- function(data_type, code) {
+  file_name <- paste0("cache/", data_type, "-", code, "_", Sys.Date(), ".RData")
+  return(file_name)
+}
+
 build_cache_directory <- function(path = "cache") {
   if (!dir.exists(path)) {
     dir.create(path, showWarnings = TRUE, recursive = FALSE, mode = "0755")
@@ -10,7 +15,7 @@ build_cache_directory <- function(path = "cache") {
 }
 
 check_cache <- function(data_type, code){
-  file_name <- paste0("cache/", data_type, "_", code, ".RData")
+  file_name <- get_cache_file_name(data_type, code)
   file_test("-f", file_name)
 }
 
@@ -25,7 +30,7 @@ build_cache <- function(data, data_type, code){
   }
 
   # Save data into cache
-  file_name <- paste0("cache/", data_type, "_", code, ".RData")
+  file_name <- get_cache_file_name(data_type, code)
   content <- data
   save(content, file = file_name, compress = TRUE, compression_level = 6)
   save.image()
@@ -33,13 +38,13 @@ build_cache <- function(data, data_type, code){
 }
 
 clean_cache <- function(data_type, code){
-  file_name <- paste0("cache/", data_type, "_", code, ".RData")
+  file_name <- get_cache_file_name(data_type, code)
   file.remove(file_name)
 }
 
 get_cache <- function(data_type, code){
   if (check_cache(data_type, code)) {
-    file_name <- paste0("cache/", data_type, "_", code, ".RData")
+    file_name <- get_cache_file_name(data_type, code)
     load(file_name)
     return(content)
   }

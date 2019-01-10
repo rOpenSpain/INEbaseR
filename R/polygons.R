0f2745c1a49249e36876f507b109d1668a333da5
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2019-01-10 13:37:25 +0000

    get_operations_with_granularity

--- R/polygons.R
+++ R/polygons.R
@@ -22,3 +22,40 @@ draw_serie <- function(serie, geographical_granularity) {
       tooltip = list(pointFormat = "{point.name}")
     )
 }
+
+get_operations_with_granularity <- function(geographical_granularity = NULL, temporal_granularity = NULL) {
+
+  # Check geographical granularity
+  if (!is.null(geographical_granularity)) {
+    if ((geographical_granularity != "PROV") && (geographical_granularity != "CCAA") && (geographical_granularity != "MUN")) {
+      stop("geographical_granularity must be one of these options: PROV, CCAA or MUN.")
+    }
+  }
+
+  # Check temporal granularity
+  if (!is.null(temporal_granularity)) {
+    if ((temporal_granularity != "Anual") && (temporal_granularity != "mensual") && (temporal_granularity != "trimestral")) {
+      stop("temporal_granularity must be one of these options: anual, mensual or trimestral")
+    }
+  }
+
+  operations <- NULL
+  all_operations <- get_operations_all()
+  for (operation in all_operations$Codigo) {
+    # Operation "ETR" (334): Error in open.connection(con, "rb") : HTTP error 500.
+    if (operation != "ETR") {
+      # Temporal granularity
+      series <- get_series_operation(operation)
+      # series$Periodicidad$Nombre
+      # Geographical granularity
+      variables <- get_variables_operation(operation)
+      if (geographical_granularity %in% variables$Codigo) {
+        operations <- c(operations, operation)
+        print(paste0("Found (", geographical_granularity, ") in: ", operation))
+      }
+    }
+  }
+
+  return(operations)
+
+}

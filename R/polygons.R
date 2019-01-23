b90fbd57740f86e8c467cf791d0e442537e54a00
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2019-01-23 12:12:46 +0000

    IMPROVED: draw_series function

--- R/polygons.R
+++ R/polygons.R
@@ -3,24 +3,17 @@
 # Author: Andres Nacimiento Garcia <andresnacimiento@gmail.com>
 # Project Director: Carlos J. Perez Gonzalez <cpgonzal@ull.es>
 
-get_polygon_file_name <- function(geographical_granularity) {
-  directory_root <- get_cache_directory_path(path = "data/geojson")
-  file_name <- paste0(directory_root, "/", geographical_granularity, ".geojson")
-  return(file_name)
-}
-
+# Example: draw_serie(25, "provincias")
 draw_serie <- function(serie, geographical_granularity) {
 
-  map <- get_polygon_file_name(geographical_granularity)
+  # Get polygon from cache
+  map <- get_polygon_rds_to_geojson("provincias")
 
+  # Represent map and series
   highchart(type = "map") %>%
-    hc_add_series(mapData = map, showInLegend = FALSE) %>%
-    hc_add_series(
-      data = NA, type = "serie",
-      dataLabels = list(enabled = FALSE),
-      name = "Airports",
-      tooltip = list(pointFormat = "{point.name}")
-    )
+    hc_chart(backgroundColor = "#161C20") %>%
+    hc_add_series(mapData = map, showInLegend = FALSE, nullColor = "#424242", borderWidth = 0)
+
 }
 
 #' @title Get operations by granularity

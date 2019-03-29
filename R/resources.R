aca7380c2cb53aa6d864ef5723b684dc9e07b631
Andrés Nacimiento <el.lobo.4ever@gmail.com>
2019-03-29 12:22:04 +0000

    Updated get_content

--- R/resources.R
+++ R/resources.R
@@ -3,7 +3,7 @@
 # Author: Andres Nacimiento Garcia <andresnacimiento@gmail.com>
 # Project Director: Carlos J. Perez Gonzalez <cpgonzal@ull.es>
 
-get_content <- function(url, loop = 0, max_iterations = 10, seconds = 60) {
+get_content <- function(url, loop = 1, max_iterations = 10, seconds = 60) {
 
   print(url)
   # Get URL content and catch errors using tryCatch
@@ -12,13 +12,13 @@ get_content <- function(url, loop = 0, max_iterations = 10, seconds = 60) {
   }, error = function(err) {
     # error handler picks up where error was generated
     print(paste("ERROR:  ",err))
-    print(paste0("[", loop, "] Waiting ", seconds, " seconds for try it again..."))
+    print(paste0("[", loop, "/", max_iterations, "] Waiting ", seconds, " seconds for try it again..."))
     Sys.sleep(seconds)
     # Try it X times
     if (loop == max_iterations) {
       return(NULL)
     } else {
-      get_content(url, loop + 1)
+      get_content(url, loop + 1, max_iterations = max_iterations, seconds = seconds)
     }
   }) # END tryCatch
 
